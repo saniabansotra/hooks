e8f9562e53302d14646486665428aed31c41de48
Sania Bansotra <saniabansotra@gmail.com>
2023-06-22 16:04:09 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const state = useState();
+  console.log(state);
+  const [count, setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+    // count++;
+    // console.log("clicked");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> {count}</h1>
+      <button onClick={IncNum}>Click me</button>
+    </>
   );
-}
-
+};
 export default App;
